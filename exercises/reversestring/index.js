dbae650a922a9e800226a0b4d116f440e8566aae
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-09-19 15:46:14 +0400

    string reverse options

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,6 +7,18 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+    // return str.split("").reverse().join("");
+
+//     let reversed = "";
+//     for (let el of str) {
+//         reversed = el + reversed;
+//     }
+//     return reversed;
+
+    // return str.split("").reduce((a, b) => b + a, "");
+
+    return str.split("").reduce((a, b) => b + a);
+    //If initialValue is not specified, accumulator is initialized to the first value in the array
 }
 
 module.exports = reverse;
