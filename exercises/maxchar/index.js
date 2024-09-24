49e4a3acfd4b475882c48c99caf31e81f8b25fb6
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-09-24 14:16:21 +0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    // for arrays
+    for (let el of str){
+        !chars[el] ? chars[el] = 1 : chars[el] = chars[el]+1
+    }
+    console.log(chars)
 
+    let maxLetter = '';
+    let maxValue = 0;
+    //for objects
+    for (let el in chars){
+       if(chars[el] > maxValue){
+           maxValue = chars[el];
+           maxLetter = el
+        }
+    }
+    console.log(maxLetter)
+    return maxLetter;
+}
 module.exports = maxChar;
