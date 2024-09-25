8fdec8db6a19ae26754effc4980e06a303289f4a
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-09-25 17:41:31 +0400

    chunk func  - v2

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,11 +9,25 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const finalArray = [];
-   while (array.length > size) {
-       finalArray.push(array.splice(0,size))
-   }
-    return finalArray.concat([array])
+   //  const chunked = [];
+   // while (array.length > size) {
+   //     chunked.push(array.splice(0,size))
+   // }
+   //  return chunked.concat([array])
+
+    const chunked = [];
+
+    for ( let el of array ) {
+        const last = chunked[chunked.length - 1];
+
+        if(!last || last.length === size){
+            chunked.push([el])
+        }
+        else {
+            last.push(el)
+        }
+    }
+    return chunked
 }
-chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)
+
 module.exports = chunk;
