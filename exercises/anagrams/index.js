5b9227c26d0a660a7b4ec80dbe6551325d4bd76d
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-10-06 16:30:27 +0400

    anagrams v2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,25 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+//
+//     const lettersOnlyA = stringA.replace(/\W/g,'').toLowerCase();
+//     const lettersOnlyB = stringB.replace(/\W/g,'').toLowerCase();
+//
+//     //возвращает число, указывающее, где должна находиться эта строка при сортировке
+//     // (до, после или в том же самом месте, что и строка, переданная в качестве параметра).
+//
+//     const sortedStringA = lettersOnlyA.split("").sort((a,b)=>{
+//        return  a.localeCompare(b)
+//     })
+//     const sortedStringB = lettersOnlyB.split("").sort((a,b)=>{
+//        return  a.localeCompare(b)
+//     })
+//
+//     return sortedStringA.join('') === sortedStringB.join("")
+//
+// }
+
 function anagrams(stringA, stringB) {
 
-    const lettersOnlyA = stringA.replace(/\W/g,'').toLowerCase();
-    const lettersOnlyB = stringB.replace(/\W/g,'').toLowerCase();
 
-    //возвращает число, указывающее, где должна находиться эта строка при сортировке
-    // (до, после или в том же самом месте, что и строка, переданная в качестве параметра).
+  function buildCharMap(str) {
+      const charMap = {};
 
-    //
-    //
-    const sortedStringA = lettersOnlyA.split("").sort((a,b)=>{
-       return  a.localeCompare(b)
-    })
-    const sortedStringB = lettersOnlyB.split("").sort((a,b)=>{
-       return  a.localeCompare(b)
-    })
+      for (let el of str.replace(/\W/g,'').toLowerCase()){
+          charMap[el] = charMap[el] + 1 || 1
+      }
 
-    return sortedStringA.join('') === sortedStringB.join("")
+      return charMap;
+  }
 
+    const aCharMap = buildCharMap(stringA)
+    const bCharMap = buildCharMap(stringB)
+
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false
+    }
+
+    for (let el in aCharMap){
+       if(  aCharMap[el] !== bCharMap[el]) {
+           return false
+       }
+    }
+    return true
 }
 
 module.exports = anagrams;
