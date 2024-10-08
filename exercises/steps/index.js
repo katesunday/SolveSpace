a82e85ce08c633efe938a16127a7010a0c3cb7b0
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-10-08 16:16:27 +0400

    steps 2v

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     let result = ""
+//     let space = ""
+//     for (let i = 1; i <= n; i++) {
+//         space += " "
+//     }
+//     let totalSpaces = null;
+//
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = i; j <=i; j++) {
+//             result += "#"
+//             totalSpaces = space.slice(j)
+//
+//         }
+//        console.log(result + totalSpaces)
+//     }
+// }
 
+function steps(n) {
+
+    for (let i = 0; i < n; i++) {
+        let result = ""
+
+        for (let k = 0; k < n; k++) {
+            if (k <= i){
+                result += '#'
+            }
+            else {
+                result += " "
+            }
+        }
+        console.log(result)
+    }
+
+
+}
+
+steps(2)
 module.exports = steps;
