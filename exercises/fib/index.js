c356a6fcb3a7bfade20afea6b519a08b4806d14e
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-10-15 15:03:39 +0400

    fibonacci recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,13 +8,32 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    let fibs = [];
-    for (let i = 0; i <= n; ++i) {
-       i-1 < 1 ? fibs.push(i) : fibs.push(fibs[i-1] + fibs[i-2]);
+// function fib(n) {
+//     let fibs = [];
+//     for (let i = 0; i <= n; ++i) {
+//        i-1 < 1 ? fibs.push(i) : fibs.push(fibs[i-1] + fibs[i-2]);
+//     }
+//     console.log(fibs)
+//     return fibs[n];
+// }
+
+function fib(n,count = 0, fibs = []) {
+
+    if(count > n){
+        console.log(fibs[n])
+        return fibs[n];
     }
-    console.log(fibs)
-    return fibs[n];
+    if(count -1 <1){
+        fibs.push(count)
+        count++
+        return fib(n,count,fibs)
+
+    }else{
+     fibs.push(fibs[count-1] + fibs[count-2])
+        count++
+        return  fib(n,count,fibs)
+    }
+
 }
 fib(7)
 module.exports = fib;
