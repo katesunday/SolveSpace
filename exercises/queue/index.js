019327ba7c0b95b30de4c0e33ecd40465265e5c9
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-10-17 17:51:29 +0400

    queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -9,7 +9,21 @@
 //     q.remove(); // returns 1;
 
 class Queue {
-
+    constructor (queue = []) {
+        this.queue = queue
+    }
+    add (item) {
+       this.queue.push(item)
+    }
+    remove () {
+        return this.queue.shift()
+    }
 }
 
+    const q = new Queue();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.remove();
+    q.remove();
 module.exports = Queue;
