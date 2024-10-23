e62394121afde308fb3e067b73205626bd9f37fe
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-10-23 13:51:55 +0400

    linked list circular fixed

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,30 +12,19 @@
 //   c.next = b;
 //   circular(l) // true
 
-const {LinkedList, Node} = require("./linkedlist");
 
 function circular(list) {
     let slow = list.head
     let fast = list.head
-    let counter = 0;
     while(fast.next && fast.next.next) {
-        if(slow === fast && counter !==0){
-            return true
-        }
         slow = slow.next
         fast = fast.next.next
-        counter++
+        if(slow === fast){
+            return true
+        }
+
     }
     return false
 }
-const l = new LinkedList();
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
 
-l.head = a;
-a.next = b;
-b.next = c;
-c.next = null;
-console.log(circular(l));
 module.exports = circular;
