3a4c2a8ebbf6ae2b8659a61be43a6096335b9492
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-11-05 12:19:28 +0400

    bst insert method contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -20,22 +20,46 @@ class Node {
     insert(data) {
         if(data < this.data && this.left){
            this.left.insert(data)
-        } else if (data < this.data ){
+        }
+        else if (data < this.data ){
             this.left = new Node(data);
-        } else if(data > this.data && this.right){
+        }
+        else if(data > this.data && this.right){
             this.right.insert(data)
-        } else if(data > this.data ){
+        }
+        else if(data > this.data ){
             this.right = new Node(data);
         }
     }
     contains(data) {
-
+        let result = null
+        if(data === this.data){
+            result = this
+            return result
+        }
+        else if(data < this.data && this.left){
+          return   this.left.contains(data)
+        }
+        else if(data > this.data && this.right){
+          return   this.right.contains(data)
+        }
+     return result
     }
 }
 
+// const node = new Node(10);
+// node.insert(5);
+// node.insert(15);
+// node.insert(17);
+// console.log(node)
 const node = new Node(10);
 node.insert(5);
 node.insert(15);
-node.insert(17);
-console.log(node)
+node.insert(20);
+node.insert(0);
+node.insert(-5);
+node.insert(3);
+
+const three = node.left.left.right;
+console.log(node.contains(333), three.data);
 module.exports = Node;
