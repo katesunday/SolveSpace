3c69047cfccf1f4853caf5db66e4e0a6b48ffdc2
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-11-06 18:40:40 +0400

    sorting bubble

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -6,9 +6,9 @@ function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length -1; j++) {
             if (arr[j] > arr[j+1]) {
-                let el = arr[j];
+                let lesser = arr[j];
                  arr[j] = arr[j+1]
-                 arr[j+1] = el;
+                 arr[j+1] = lesser;
             }
         }
     }
