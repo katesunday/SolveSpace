1b05b3ce63ecc8b15481cdb7fa4faa8e765dd40d
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-11-07 16:45:43 +0400

    sorting merge method

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -57,6 +57,7 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+    //works for 2 sorted arrays
     let result = [];
     while(left.length || right.length){
         if(left[0] < right[0]){
